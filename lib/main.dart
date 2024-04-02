be46d10aafabf3a3cdb8afbf239f90ec9b986eac
Nathalia-Valentim <126782064+Nathalia-Valentim@users.noreply.github.com>
2024-04-02 11:02:00 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:proj_login/userInterface/pages/login.dart';
+
+void main() => runApp( MyApp());
+
+class MyApp extends MaterialApp {
+   MyApp({super.key}) : super(
+    home: Login(), 
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      brightness: Brightness.dark
+    ),
+  );
+}
